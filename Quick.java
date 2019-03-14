e3f29744f6a707e6729e20317df522e8e5939be1
Derek Lao <dlao00@stuy.edu>
2019-03-14 01:27:27 -0400

    added untested recursive quickselect

--- Quick.java
+++ Quick.java
@@ -2,21 +2,23 @@ import java.util.*;
 import java.io.*;
 
 public class Quick{
+  /**@return the index of the final position of the pivot element.
+ */
   public static int partition ( int [] data, int start, int end){
     int random = ((int) (Math.random() * 100) ) % data.length;
     int pivot = data[random];
     int holder = data[start];
     data[start] = pivot;
     data[random] = holder;
-    System.out.println("The index is " + random);
-    System.out.println("The pivot value is " + pivot + "\n");
+    // System.out.println("The index is " + random);
+    // System.out.println("The pivot value is " + pivot + "\n");
     int s = start + 1;
     int e = end;
     for(int i = 0; i < data.length && s < data.length && e > -1; i ++)
     {
-      System.out.println("Before swapping:");
-      System.out.println("Index s is " + s + "  Index e is " + e);
-      System.out.println("The array is now " + Arrays.toString(data));
+      // System.out.println("Before swapping:");
+      // System.out.println("Index s is " + s + "  Index e is " + e);
+      // System.out.println("The array is now " + Arrays.toString(data));
       if(data[s] > pivot)
       {
         holder = data[s];
@@ -42,14 +44,38 @@ public class Quick{
         }
         i = data.length;
       }
-      System.out.println("After swapping:");
-      System.out.println("Index s is " + s + "  Index e is " + e);
-      System.out.println("The array is now " + Arrays.toString(data) + "\n");
+      // System.out.println("After swapping:");
+      // System.out.println("Index s is " + s + "  Index e is " + e);
+      // System.out.println("The array is now " + Arrays.toString(data) + "\n");
     }
     start = s;
     end = e;
     return random;
   }
+
+  /*return the value that is the kth smallest value of the array.
+  */
+  public static int quickselect(int[] data, int k){
+    return quickselectH(data,k,0,data.length - 1);
+  }
+
+
+  private static int quickselectH(int[] data, int k, int splice1, int splice2){
+    int holder = partition(data,splice1,splice2);
+    if(holder < k)
+    {
+      return quickselectH(data,k,holder,splice2);
+    }
+    else if(holder > k)
+    {
+      return quickselectH(data,k,splice1,holder);
+    }
+    else
+    {
+      return data[k];
+    }
+  }
+
 
   public static void main(String[] args){
     int[] numArray = new int[10];
@@ -58,9 +84,8 @@ public class Quick{
       numArray[i] = (int) (Math.random()*1000) % 20;
     }
     System.out.println("The original array is: " + Arrays.toString(numArray));
-    partition(numArray,0,numArray.length - 1);
-    System.out.println("The new array is: " + Arrays.toString(numArray));
-
+    // partition(numArray,0,numArray.length - 1);
+    System.out.println("quickselect for the fourth smallest element: " + quickselect(numArray,4));
   }
 
 
