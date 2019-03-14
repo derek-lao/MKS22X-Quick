7db3a4c5e20eea474587b7f0c6cfea1839d6f4dd
Derek Lao <dlao00@stuy.edu>
2019-03-14 18:57:49 -0400

    probably fixed something. fixed random always being 0 if it weren't comparing hi and lo. But for some reason random should never go to 0. I will actually chnge random back to 0 for debugging

--- Quick.java
+++ Quick.java
@@ -104,8 +104,7 @@ public class Quick{
     {
       return start;
     }
-    // int random = (((int) (Math.random() * 100) ) % (end - start + 1)) + start;
-    int random = 0;
+    int random = start;
     int lo = data[start];
     int hi = data[end];
     int med = data[((start + end) / 2)];
@@ -125,15 +124,15 @@ public class Quick{
     int holder = data[start];
     data[start] = pivot;
     data[random] = holder;
-    // System.out.println("The index is " + random);
-    // System.out.println("The pivot value is " + pivot + "\n");
+    System.out.println("The index is " + random);
+    System.out.println("The pivot value is " + pivot + "\n");
     int st = start + 1;
     int e = end;
     for(int i = 0; i < data.length && st < data.length && e > -1; i ++)
     {
-      // System.out.println("Before swapping:");
-      // System.out.println("Index st is " + st + "  Index e is " + e);
-      // System.out.println("The array is now " + Arrays.toString(data));
+      System.out.println("Before swapping:");
+      System.out.println("Index st is " + st + "  Index e is " + e);
+      System.out.println("The array is now " + Arrays.toString(data));
       if(e <= st)
       {
         if(data[st] > pivot)
@@ -149,9 +148,9 @@ public class Quick{
           random = st;
         }
         i = data.length;
-        // System.out.println("After swapping:");
-        // System.out.println("Index st is " + st + "  Index e is " + e);
-        // System.out.println("The array is now " + Arrays.toString(data) + "\n");
+        System.out.println("After final swapping:");
+        System.out.println("Index st is " + st + "  Index e is " + e);
+        System.out.println("The array is now " + Arrays.toString(data) + "\n");
         continue;
       }
       if(data[st] > pivot)
@@ -160,21 +159,22 @@ public class Quick{
         data[st] = data[e];
         data[e] = holder;
         e--;
-        // System.out.println("After swapping:");
-        // System.out.println("Index st is " + st + "  Index e is " + e);
-        // System.out.println("The array is now " + Arrays.toString(data) + "\n");
+        System.out.println("After swapping:");
+        System.out.println("Index st is " + st + "  Index e is " + e);
+        System.out.println("The array is now " + Arrays.toString(data) + "\n");
         continue;
       }
       if(data[st] <= pivot)
       {
         st++;
-        // System.out.println("After swapping:");
-        // System.out.println("Index st is " + st + "  Index e is " + e);
-        // System.out.println("The array is now " + Arrays.toString(data) + "\n");
+        System.out.println("After swapping:");
+        System.out.println("Index st is " + st + "  Index e is " + e);
+        System.out.println("The array is now " + Arrays.toString(data) + "\n");
         continue;
       }
     }
-    // System.out.println("The index of the pivot is " + random);
+    System.out.println("The index of the pivot is " + random);
+    System.out.println("Finished this partition call.\n");
     return random;
   }
 
@@ -206,42 +206,53 @@ public class Quick{
   /*Modify the array to be in increasing order.
   */
   public static void quicksort(int[] data){
-    quicksortH(data,0,data.length - 1);
-  }
-
-  private static void quicksortH(int[] data,int splice1, int splice2){
     System.out.println("This is the current array: " + Arrays.toString(data));
-    int holder = partition(data,splice1,splice2);
-    if(splice1 < splice2)
+    // quicksortH(data,0,data.length - 1);
+    for(int i = 0; i < data.length; i ++)
     {
-      if(holder != 0)
-      {
-        quicksortH(data,splice1,holder - 1);
-      }
-      if(holder != data.length - 1)
-      {
-        quicksortH(data,holder + 1,splice2);
-      }
+      // data[i] = quickselectH(data,i,i,data.length-1);
+      data[i] = quickselectH(data,i,i,data.length - 1);
     }
     System.out.println("This is the new array: " + Arrays.toString(data));
   }
 
+  private static void quicksortH(int[] data,int splice1, int splice2){
+    // System.out.println("This is the current array: " + Arrays.toString(data));
+    // int holder = partition(data,splice1,splice2);
+    // if(splice1 < splice2)
+    // {
+    //   if(holder != 0)
+    //   {
+    //     quicksortH(data,splice1,holder - 1);
+    //   }
+    //   if(holder != data.length - 1)
+    //   {
+    //     quicksortH(data,holder + 1,splice2);
+    //   }
+    // }
+    // System.out.println("This is the new array: " + Arrays.toString(data));
+  }
+
 
   public static void main(String[] args){
-    int[] numArray = new int[10];
+    int[] randomArray = new int[(int) Math.random() * 1000 % 20];
     int[][] problemArrays = new int[10][10];
-    // problemArrays[0] = {10, 11, 15, 11, 15, 12, 10, 14, 11, 10};
-    for(int i = 0; i < numArray.length; i ++)
+    int[] issueArray = {10, 11, 15, 11, 15, 12, 10, 14, 11, 10};
+    problemArrays[0] = issueArray;
+    for(int i = 0; i < 1; i ++)
     {
-      numArray[i] = (int) (Math.random()*1000) % 20;
+      int[] thisArray = problemArrays[i];
+      System.out.println("The original array is: " + Arrays.toString(thisArray));
+      quickselect(thisArray,4);
+      System.out.println("The new array is: " + Arrays.toString(thisArray));
     }
-    System.out.println("The original array is: " + Arrays.toString(numArray));
+    System.out.println("The original array is: " + Arrays.toString(randomArray));
     // partition(numArray,0,numArray.length - 1);
     // System.out.println("quickselect for the element at 4th index: " + quickselect(numArray,4));
 
     // System.out.println("quickselect for the 0th smallest element: " + quickselect(numArray,0));
-    quicksort(numArray);
-    System.out.println("The new sorted array is: " + Arrays.toString(numArray));
+    // quicksort(numArray);
+    System.out.println("The array is: " + Arrays.toString(randomArray));
   }
 
 
