2ebdf1fd11693eab41728080216e08aad3f16571
Derek Lao <dlao00@stuy.edu>
2019-03-14 02:41:33 -0400

    added improved partition, tested and worked

--- Quick.java
+++ Quick.java
@@ -9,7 +9,23 @@ public class Quick{
     {
       return start;
     }
-    int random = (((int) (Math.random() * 100) ) % (end - start + 1)) + start;
+    // int random = (((int) (Math.random() * 100) ) % (end - start + 1)) + start;
+    int random = 0;
+    int lo = data[start];
+    int hi = data[end];
+    int med = data[((start + end) / 2)];
+    if(lo <= hi && lo >= med)
+    {
+      random = start;
+    }
+    else if(hi <= lo && hi >= med)
+    {
+      random = end;
+    }
+    else if(med <= lo && med >= hi)
+    {
+      random = (start + end) / 2;
+    }
     int pivot = data[random];
     int holder = data[start];
     data[start] = pivot;
@@ -20,9 +36,9 @@ public class Quick{
     int e = end;
     for(int i = 0; i < data.length && s < data.length && e > -1; i ++)
     {
-      System.out.println("Before swapping:");
-      System.out.println("Index s is " + s + "  Index e is " + e);
-      System.out.println("The array is now " + Arrays.toString(data));
+      // System.out.println("Before swapping:");
+      // System.out.println("Index s is " + s + "  Index e is " + e);
+      // System.out.println("The array is now " + Arrays.toString(data));
       if(e <= s)
       {
         if(data[s] > pivot)
@@ -49,17 +65,17 @@ public class Quick{
         data[s] = data[e];
         data[e] = holder;
         e--;
-        System.out.println("After swapping:");
-        System.out.println("Index s is " + s + "  Index e is " + e);
-        System.out.println("The array is now " + Arrays.toString(data) + "\n");
+        // System.out.println("After swapping:");
+        // System.out.println("Index s is " + s + "  Index e is " + e);
+        // System.out.println("The array is now " + Arrays.toString(data) + "\n");
         continue;
       }
       if(data[s] <= pivot)
       {
         s++;
-        System.out.println("After swapping:");
-        System.out.println("Index s is " + s + "  Index e is " + e);
-        System.out.println("The array is now " + Arrays.toString(data) + "\n");
+        // System.out.println("After swapping:");
+        // System.out.println("Index s is " + s + "  Index e is " + e);
+        // System.out.println("The array is now " + Arrays.toString(data) + "\n");
         continue;
       }
     }
@@ -68,7 +84,8 @@ public class Quick{
     return random;
   }
 
-  /*return the value that is the kth smallest value of the array.
+
+  /*return the value that is the kth smallest value of the array. k = 0 is smallest
   */
   public static int quickselect(int[] data, int k){
     int answer = quickselectH(data,k,0,data.length - 1);
@@ -76,21 +93,26 @@ public class Quick{
     return answer;
   }
 
-
   private static int quickselectH(int[] data, int k, int splice1, int splice2){
     int holder = partition(data,splice1,splice2);
-    if(holder < k - 1)
+    if(holder < k)
     {
       return quickselectH(data,k,holder + 1,splice2);
     }
-    else if(holder > k - 1)
+    else if(holder > k)
     {
       return quickselectH(data,k,splice1,holder - 1);
     }
     else
     {
-      return data[k - 1];
+      return data[k];
     }
+  }
+
+  /*Modify the array to be in increasing order.
+  */
+  public static void quicksort(int[] data){
+
   }
 
 
@@ -102,8 +124,10 @@ public class Quick{
     }
     System.out.println("The original array is: " + Arrays.toString(numArray));
     // partition(numArray,0,numArray.length - 1);
-    System.out.println("quickselect for the fourth smallest element: " + quickselect(numArray,4));
+    // System.out.println("quickselect for the fourth smallest element: " + quickselect(numArray,4));
     // System.out.println("quickselect for the ninth smallest element: " + quickselect(numArray,9));
+
+    System.out.println("quickselect for the 0th smallest element: " + quickselect(numArray,0));
   }
 
 
